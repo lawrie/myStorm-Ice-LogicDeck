a99f1efa34889bd53c8fea939970066f1bb30d35
lawrie <lawrie.griffiths@ntlword.com>
2022-06-04 16:16:23 +0100

    Tidied qbus peripheral test

--- HDL/Amaranth_Examples/qbus_periph.py
+++ HDL/Amaranth_Examples/qbus_periph.py
@@ -4,73 +4,64 @@ from amaranth_stdio.serial import AsyncSerial
 from amaranth.lib.fifo import SyncFIFOBuffered
 
 from mystorm_boards.icelogicbus import *
+
 from HDL.Tiles.seven_seg_tile import tile_resources
-from qspimem import QspiMem
 from HDL.Misc.pll import PLL
 
+from qspimem import QspiMem
 from sevensegtile import SevenSegmentTile
 
 BLADE = 1
 TILE = 3
+PMOD = 5
 
 led_blade = [
     Resource("leds6", 0,
-                Subsignal("leds",
-                          Pins("1 2 3 4 5 6", dir="o", invert=True, conn=("blade", BLADE)),
-                          Attrs(IO_STANDARD="SB_LVCMOS")
-                          )
-             )
+             Subsignal("leds", Pins("1 2 3 4 5 6", dir="o", invert=True, conn=("blade", BLADE))),
+             Attrs(IO_STANDARD="SB_LVCMOS"))
 ]
 
-PMOD = 5
-
+# External uart connected to Pmod
 uart_pmod = [
     Resource("ext_uart", 0,
-             Subsignal("tx", Pins("10", dir="o", conn=("pmod", PMOD))),
-             Subsignal("rx", Pins("4", dir="i", conn=("pmod", PMOD))),
-             Subsignal("gnd", Pins("9", dir="o", conn=("pmod", PMOD))),
+             Subsignal("tx",  Pins("10", dir="o", conn=("pmod", PMOD))),
+             Subsignal("rx",  Pins("4",  dir="i", conn=("pmod", PMOD))),
+             Subsignal("gnd", Pins("9",  dir="o", conn=("pmod", PMOD))),
              Attrs(IO_STANDARD="SB_LVCMOS"))
 ]
 
 
-
-
-
 class QbusPeriph(Elaboratable):
     def elaborate(self, platform):
-        qspi  = platform.request("qspi")
-        qss   = qspi.cs
-        qck   = qspi.clk
-        qd_i  = qspi.data.i
-        qd_o  = qspi.data.o
-        qd_oe = qspi.data.oe
+        # Get pins
+        qspi = platform.request("qspi")
         leds6 = platform.request("leds6")
         led = platform.request("led")
 
         m = Module()
 
         # Clock generator.
-        CLK_FREQ = 1e8
-        clk_in = platform.request(platform.default_clk, dir='-')[0]
+        clk_freq = 1e8
+        clk_in = platform.request(platform.default_clk)
         # Create a Pll for 100Mhz clock
         m.submodules.pll = pll = PLL(freq_in_mhz=int(platform.default_clk_frequency / 1e6),
-                                     freq_out_mhz=int(CLK_FREQ / 1e6),
+                                     freq_out_mhz=int(clk_freq / 1e6),
                                      domain_name="sync")
         # Set the sync domain to the pll domain
         m.domains.sync = cd_sync = pll.domain
         m.d.comb += pll.clk_pin.eq(clk_in)
-        platform.add_clock_constraint(cd_sync.clk, CLK_FREQ)
+        platform.add_clock_constraint(cd_sync.clk, clk_freq)
 
         # Add QspiMem submodule
         m.submodules.qspimem = qspimem = QspiMem()
 
         # Connect pins
         m.d.comb += [
-            qspimem.qss.eq(qss),
-            qspimem.qck.eq(qck),
-            qspimem.qd_i.eq(qd_i),
-            qd_o.eq(qspimem.qd_o),
-            qd_oe.eq(qspimem.qd_oe)
+            qspimem.qss.eq(qspi.cs),
+            qspimem.qck.eq(qspi.clk),
+            qspimem.qd_i.eq(qspi.data.i),
+            qspi.data.o.eq(qspimem.qd_o),
+            qspi.data.oe.eq(qspimem.qd_oe)
         ]
 
         # Add 7-segment display tile controller
@@ -91,7 +82,7 @@ class QbusPeriph(Elaboratable):
 
         # Create uart
         ext_uart = platform.request("ext_uart")
-        divisor = int(CLK_FREQ // 115200)
+        divisor = int(clk_freq // 115200)
 
         m.submodules.serial = serial = AsyncSerial(divisor=divisor)
 
@@ -108,17 +99,6 @@ class QbusPeriph(Elaboratable):
 
         # Write to peripherals
 
-        # Uart
-        m.d.comb += [
-            # Write received data
-            serial.tx.data.eq(fifo.r_data),
-            # Write data to fifo when received
-            fifo.w_data.eq(qspimem.dout),
-            fifo.w_en.eq(qspimem.wr & (qspimem.addr >= 3)),
-            fifo.r_en.eq(serial.tx.rdy),
-            serial.tx.ack.eq(fifo.r_rdy & serial.tx.rdy),
-        ]
-
         # 7-segment lower byte
         with m.If(qspimem.wr & (qspimem.addr == 0)):
             m.d.sync += display.eq(qspimem.dout)
@@ -134,7 +114,19 @@ class QbusPeriph(Elaboratable):
                 led.eq(qspimem.dout[7])
             ]
 
+        # Uart
+        m.d.comb += [
+            # Write received data
+            serial.tx.data.eq(fifo.r_data),
+            # Write data to fifo when received
+            fifo.w_data.eq(qspimem.dout),
+            fifo.w_en.eq(qspimem.wr & (qspimem.addr >= 3)),
+            fifo.r_en.eq(serial.tx.rdy),
+            serial.tx.ack.eq(fifo.r_rdy & serial.tx.rdy),
+        ]
+
         return m
+
 
 def synth():
     platform = IceLogicBusPlatform()
@@ -142,9 +134,13 @@ def synth():
     platform.add_resources(led_blade)
     platform.add_resources(uart_pmod)
     platform.build(QbusPeriph(), nextpnr_opts="--timing-allow-fail", do_program=True)
-    print("Sending QSPI data")
-    platform.bus_send(bytearray(b'\x03\x00\x00\x00\x00\x00\x00\x00\x0a\x42\x06\xb8Hello\r\n'))
-    print("Data sent")
+    # Send bus command
+    addr = 0
+    data = b'\x42\x06\xb8Hello World!\r\n'
+    command = b'\x03' + addr.to_bytes(4, 'big') + len(data).to_bytes(4, 'big') + data
+    print("Sending command: ", command)
+    platform.bus_send(command)
+
 
 if __name__ == "__main__":
     synth()
