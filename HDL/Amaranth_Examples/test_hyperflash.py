a74ffc212f12ed62a125442d1e331d0cd3934377
lawrie <lawrie.griffiths@ntlword.com>
2022-07-20 17:59:06 +0100

    Read programs up to 16KB

--- HDL/Amaranth_Examples/test_hyperflash.py
+++ HDL/Amaranth_Examples/test_hyperflash.py
@@ -14,19 +14,47 @@ led_blade = [
 ]
 
 
+def readbios():
+    """ Read bios.bin into an array of integers """
+    f = open("bios.bin", "rb")
+    l = []
+    while True:
+        b = f.read(4)
+        if b:
+            l.append(int.from_bytes(b, "little"))
+        else:
+            break
+    f.close()
+    return l
+
+
 class TestHyperflash(Elaboratable):
     def elaborate(self, platform):
         m = Module()
+
+        bios = readbios()
+        mem = Memory(width=32, depth=4096)
+        mem.init = bios
+        print("bios length:", len(bios))
+        print("lines:", (len(bios) + 127) // 128)
+        m.submodules.rp = rp = mem.read_port()
 
         hbus = platform.request("hyperbus", 0)
         pins = HyperbusPins()
 
         m.submodules.hfw = hfw = HyperflashWrite(pins=pins, init_latency=16)
 
+        size = Signal(13)
+        lines = Signal(6)
+        lines_done = Signal(6, reset=0)
+        cmd = Signal(2, reset=1)  # start with erase segment
+
         m.d.comb += [
+            size.eq(len(bios) + 127),
+            lines.eq(size[7:]),           # Number of 512 byte lines
             hbus.clk.o.eq(pins.clk_o),
-            hbus.cs.o[1].eq(pins.csn_o),
-            hbus.cs.o[0].eq(1),
+            hbus.cs.o[1].eq(pins.csn_o),  # HyperFlash
+            hbus.cs.o[0].eq(1),           # Disable HyperRAM
 
             hbus.rd.o.eq(pins.rwds_o),
             hbus.rd.oe.eq(pins.rwds_oe),
@@ -35,20 +63,32 @@ class TestHyperflash(Elaboratable):
             hbus.data.o.eq(pins.dq_o),
             hbus.data.oe.eq(pins.dq_oe),
             pins.dq_i.eq(hbus.data.i),
-            hfw.start_addr.eq(0),
-            hfw.len.eq(256)
+            hfw.start_addr.eq(Cat(C(0, 9), lines_done)),
+            hfw.len.eq(256),
+            rp.addr.eq(hfw.addr[1:]),
+            hfw.cmd.eq(cmd)
         ]
+
+        # Register the done signal
+        r_done = Signal()
+        m.d.sync += r_done.eq(hfw.done)
 
         # Set data to word address
         with m.If(hfw.next):
-            m.d.sync += hfw.din.eq(hfw.addr)
+            m.d.sync += hfw.din.eq(Mux(hfw.addr[0], rp.data[:16], rp.data[16:]))
 
-        with m.If(hfw.done):
-            m.d.sync += hfw.cmd.eq(0)
-        with m.Else():
-            m.d.sync += hfw.cmd.eq(2)
+        # Do an erase command followed by series of write-buffer commands
+        with m.If(hfw.done & ~r_done):
+            with m.If((lines_done == lines - 1) & (cmd == 2)):
+                m.d.sync += cmd.eq(0)   # All lines done
+            with m.Else():
+                with m.If(cmd == 2):
+                    m.d.sync += lines_done.eq(lines_done + 1)
+                with m.Else():
+                    m.d.sync += cmd.eq(2)
 
         return m
+
 
 if __name__ == "__main__":
     platform = IceLogicBusPlatform()
